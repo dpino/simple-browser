2d95893d71e4e2a08b5b51cbfd9ff581994857f9
Diego Pino Garcia <dpino@igalia.com>
2014-04-15 17:53:33 +0200

    Format code

--- src/com/example/simplebrowser/MainActivity.java
+++ src/com/example/simplebrowser/MainActivity.java
@@ -19,114 +19,114 @@ public class MainActivity extends Activity {
 
     private static final String DEFAULT_URL = "http://www.google.com";
 
-	private WebView mWebView;
-	private EditText mAddressBar;
-	private ProgressBar mProgressBar;
-	
+    private WebView mWebView;
+    private EditText mAddressBar;
+    private ProgressBar mProgressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         this.requestWindowFeature(getWindow().FEATURE_NO_TITLE);
-        
+
         setContentView(R.layout.activity_main);
-        
+
         wireComponents();
         initializeWebView();
         initializeAddressBar();
     }
 
-	private void wireComponents() {
+    private void wireComponents() {
         mWebView = (WebView) findViewById(R.id.webview);
         mAddressBar = (EditText) findViewById(R.id.addressbar);
         mProgressBar = (ProgressBar) findViewById(R.id.progressbar);
-	}
+    }
 
-	private void initializeWebView() {
-		// Force links and redirects to open in the WebView instead of in a
-		// browser
-		mWebView.setWebViewClient(new MyWebViewClient());
-		mWebView.setWebChromeClient(new MyWebChromeClient());
+    private void initializeWebView() {
+        // Force links and redirects to open in the WebView instead of in a
+        // browser
+        mWebView.setWebViewClient(new MyWebViewClient());
+        mWebView.setWebChromeClient(new MyWebChromeClient());
 
-		// Enable JavaScript
+        // Enable JavaScript
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-	}
-	
-	private void hideProgressBar() {
-		mProgressBar.setVisibility(View.GONE);
-	}
+    }
 
-	private void showProgressBar() {
-		mProgressBar.setVisibility(View.VISIBLE);
-	}
+    private void hideProgressBar() {
+        mProgressBar.setVisibility(View.GONE);
+    }
 
-	private class MyWebViewClient extends WebViewClient {
+    private void showProgressBar() {
+        mProgressBar.setVisibility(View.VISIBLE);
+    }
 
-		@Override
-		public void onPageFinished(WebView view, String url) {
-			super.onPageFinished(view, url);
-			MainActivity.this.hideProgressBar();
-		}
+    private class MyWebViewClient extends WebViewClient {
 
-		@Override
-		public void onPageStarted(WebView view, String url, Bitmap favicon) {
-			super.onPageStarted(view, url, favicon);
-			MainActivity.this.showProgressBar();
-		}
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            MainActivity.this.hideProgressBar();
+        }
 
-	};
-	
-	private class MyWebChromeClient extends WebChromeClient {
-		@Override
-		public void onProgressChanged(WebView view, int newProgress) {
-			MainActivity.this.setProgressValue(newProgress);
-			super.onProgressChanged(view, newProgress);
-		}
-	}
-	
-	public void setProgressValue(int progress) {
-        mProgressBar.setProgress(progress);        
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            MainActivity.this.showProgressBar();
+        }
+
+    };
+
+    private class MyWebChromeClient extends WebChromeClient {
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            MainActivity.this.setProgressValue(newProgress);
+            super.onProgressChanged(view, newProgress);
+        }
+    }
+
+    public void setProgressValue(int progress) {
+        mProgressBar.setProgress(progress);
     }
 
     private void initializeAddressBar() {
-    	mAddressBar.setOnKeyListener(new OnKeyListener() {
+        mAddressBar.setOnKeyListener(new OnKeyListener() {
 
-			@Override
-			public boolean onKey(View v, int keyCode, KeyEvent event) {
-				if ((event.getAction() == KeyEvent.ACTION_DOWN)
-						&& (keyCode == KeyEvent.KEYCODE_ENTER)) {
-					hideKeyboard();
-					loadUrl(getUrlAddress());
-				}
-			return false;
-			}
-    		
-			private void hideKeyboard() {
-				InputMethodManager in = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-				in.hideSoftInputFromWindow(
-						mAddressBar.getApplicationWindowToken(),
-						InputMethodManager.HIDE_NOT_ALWAYS);
-			}
-			
-    	});    	
-    	setUrlAddress(DEFAULT_URL);
-	}
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN)
+                        && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    hideKeyboard();
+                    loadUrl(getUrlAddress());
+                }
+                return false;
+            }
 
-    private void setUrlAddress(String url) {
-    	mAddressBar.setText(url);
-		loadUrl(url);    	
+            private void hideKeyboard() {
+                InputMethodManager in = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                in.hideSoftInputFromWindow(
+                        mAddressBar.getApplicationWindowToken(),
+                        InputMethodManager.HIDE_NOT_ALWAYS);
+            }
+
+        });
+        setUrlAddress(DEFAULT_URL);
     }
 
-	private void loadUrl(String url) {
-		mProgressBar.setVisibility(View.VISIBLE);
-		mWebView.loadUrl(url);
-		mWebView.requestFocus();
-		mProgressBar.setProgress(0);
-	}
+    private void setUrlAddress(String url) {
+        mAddressBar.setText(url);
+        loadUrl(url);
+    }
+
+    private void loadUrl(String url) {
+        mProgressBar.setVisibility(View.VISIBLE);
+        mWebView.loadUrl(url);
+        mWebView.requestFocus();
+        mProgressBar.setProgress(0);
+    }
 
     private String getUrlAddress() {
-    	return mAddressBar.getText().toString();
+        return mAddressBar.getText().toString();
     }
 
 }
